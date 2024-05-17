bbbf80d98d1c5055100febd520ae390ea7afcf05
PitBosch <62173411+PitBosch@users.noreply.github.com>
2024-05-17 12:21:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import streamlit as st
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
-from langchain import OpenAI, VectorDBQA
+from langchain_community.llms import OpenAI
+from langchain.chains import VectorDBQA
 from langchain.chains import RetrievalQAWithSourcesChain
 from langchain_community.chat_models import ChatOpenAI
 import PyPDF2
