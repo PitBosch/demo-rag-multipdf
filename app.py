cd87f4bdd6639b535902dcd0dfff7e9b765070c5
PitBosch <62173411+PitBosch@users.noreply.github.com>
2024-05-17 13:05:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,78 +1,138 @@
+import os, tempfile
+import pinecone
+from pathlib import Path
+
+from langchain.chains import RetrievalQA, ConversationalRetrievalChain
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain import OpenAI
+from langchain.llms.openai import OpenAIChat
+from langchain.document_loaders import DirectoryLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import Chroma, Pinecone
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.memory import ConversationBufferMemory
+from langchain.memory.chat_message_histories import StreamlitChatMessageHistory
+
 import streamlit as st
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import OpenAIEmbeddings
-from langchain_community.vectorstores import Chroma
-from langchain_community.llms import OpenAI
-from langchain.chains import VectorDBQA
-from langchain.chains import RetrievalQAWithSourcesChain
-from langchain_community.chat_models import ChatOpenAI
-import PyPDF2
-import os
-os.environ["OPENAI_API_KEY"] = st.secrets["openai_api_key"]
-# This block of code imports pysqlite3 and sets it to sqlite3 to avoid potential conflicts
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
-# Function to extract text from PDF files and return as a list
-def read_and_textify(files):
-    text_list = []
-    sources_list = []
-    for file in files:
-        pdfReader = PyPDF2.PdfReader(file)
-        for i in range(len(pdfReader.pages)):
-            pageObj = pdfReader.pages[i]
-            text = pageObj.extract_text()
-            text_list.append(text)
-            sources_list.append(file.name + "_page_" + str(i))
-    return [text_list, sources_list]
 
-st.set_page_config(layout="centered", page_title="GoldDigger")
-st.header("GoldDigger")
-st.write("---")
+TMP_DIR = Path(__file__).resolve().parent.joinpath('data', 'tmp')
+LOCAL_VECTOR_STORE_DIR = Path(__file__).resolve().parent.joinpath('data', 'vector_store')
 
-# File uploader
-uploaded_files = st.file_uploader("Upload documents", accept_multiple_files=True, type=["txt", "pdf"])
-st.write("---")
+st.set_page_config(page_title="RAG")
+st.title("Retrieval Augmented Generation Engine")
 
-if not uploaded_files:
-    st.info("Upload files to analyze")
-else:
-    st.write(f"{len(uploaded_files)} document(s) loaded..")
-    textify_output = read_and_textify(uploaded_files)
-    
-    documents = textify_output[0]
-    sources = textify_output[1]
-    
-    # Extract embeddings
-    key = os.environ['OPENAI_API_KEY']
-    embeddings = OpenAIEmbeddings(openai_api_key=key)
-    
-    # Vector store with metadata (page numbers)
-    vStore = Chroma.from_texts(documents, embeddings, metadatas=[{"source": s} for s in sources])
-    
-    # Deciding model
-    model_name = "gpt-3.5-turbo"
-    # model_name = "gpt-4"
-    
-    retriever = vStore.as_retriever()
-    retriever.search_kwargs = {'k': 2}
-    
-    # Initiate model
-    llm = OpenAI(model_name=model_name, openai_api_key=key, streaming=True)
-    model = RetrievalQAWithSourcesChain.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
-    
-    st.header("Ask your data")
-    user_q = st.text_area("Enter your questions here")
-    
-    if st.button("Get Response"):
+
+def load_documents():
+    loader = DirectoryLoader(TMP_DIR.as_posix(), glob='**/*.pdf')
+    documents = loader.load()
+    return documents
+
+def split_documents(documents):
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.split_documents(documents)
+    return texts
+
+def embeddings_on_local_vectordb(texts):
+    vectordb = Chroma.from_documents(texts, embedding=OpenAIEmbeddings(),
+                                     persist_directory=LOCAL_VECTOR_STORE_DIR.as_posix())
+    vectordb.persist()
+    retriever = vectordb.as_retriever(search_kwargs={'k': 7})
+    return retriever
+
+def embeddings_on_pinecone(texts):
+    pinecone.init(api_key=st.session_state.pinecone_api_key, environment=st.session_state.pinecone_env)
+    embeddings = OpenAIEmbeddings(openai_api_key=st.session_state.openai_api_key)
+    vectordb = Pinecone.from_documents(texts, embeddings, index_name=st.session_state.pinecone_index)
+    retriever = vectordb.as_retriever()
+    return retriever
+
+def query_llm(retriever, query):
+    qa_chain = ConversationalRetrievalChain.from_llm(
+        llm=OpenAIChat(openai_api_key=st.session_state.openai_api_key),
+        retriever=retriever,
+        return_source_documents=True,
+    )
+    result = qa_chain({'question': query, 'chat_history': st.session_state.messages})
+    result = result['answer']
+    st.session_state.messages.append((query, result))
+    return result
+
+def input_fields():
+    #
+    with st.sidebar:
+        #
+        if "openai_api_key" in st.secrets:
+            st.session_state.openai_api_key = st.secrets.openai_api_key
+        else:
+            st.session_state.openai_api_key = st.text_input("OpenAI API key", type="password")
+        #
+        if "pinecone_api_key" in st.secrets:
+            st.session_state.pinecone_api_key = st.secrets.pinecone_api_key
+        else: 
+            st.session_state.pinecone_api_key = st.text_input("Pinecone API key", type="password")
+        #
+        if "pinecone_env" in st.secrets:
+            st.session_state.pinecone_env = st.secrets.pinecone_env
+        else:
+            st.session_state.pinecone_env = st.text_input("Pinecone environment")
+        #
+        if "pinecone_index" in st.secrets:
+            st.session_state.pinecone_index = st.secrets.pinecone_index
+        else:
+            st.session_state.pinecone_index = st.text_input("Pinecone index name")
+    #
+    st.session_state.pinecone_db = st.toggle('Use Pinecone Vector DB')
+    #
+    st.session_state.source_docs = st.file_uploader(label="Upload Documents", type="pdf", accept_multiple_files=True)
+    #
+
+
+def process_documents():
+    if not st.session_state.openai_api_key or not st.session_state.pinecone_api_key or not st.session_state.pinecone_env or not st.session_state.pinecone_index or not st.session_state.source_docs:
+        st.warning(f"Please upload the documents and provide the missing fields.")
+    else:
         try:
-            with st.spinner("Model is working on it..."):
-                result = model({"question": user_q}, return_only_outputs=True)
-                st.subheader('Your response:')
-                st.write(result['answer'])
-                st.subheader('Source pages:')
-                st.write(result['sources'])
+            for source_doc in st.session_state.source_docs:
+                #
+                with tempfile.NamedTemporaryFile(delete=False, dir=TMP_DIR.as_posix(), suffix='.pdf') as tmp_file:
+                    tmp_file.write(source_doc.read())
+                #
+                documents = load_documents()
+                #
+                for _file in TMP_DIR.iterdir():
+                    temp_file = TMP_DIR.joinpath(_file)
+                    temp_file.unlink()
+                #
+                texts = split_documents(documents)
+                #
+                if not st.session_state.pinecone_db:
+                    st.session_state.retriever = embeddings_on_local_vectordb(texts)
+                else:
+                    st.session_state.retriever = embeddings_on_pinecone(texts)
         except Exception as e:
             st.error(f"An error occurred: {e}")
-            st.error("Oops, the GPT response resulted in an error :( Please try again with a different question.")
+
+def boot():
+    #
+    input_fields()
+    #
+    st.button("Submit Documents", on_click=process_documents)
+    #
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+    #
+    for message in st.session_state.messages:
+        st.chat_message('human').write(message[0])
+        st.chat_message('ai').write(message[1])    
+    #
+    if query := st.chat_input():
+        st.chat_message("human").write(query)
+        response = query_llm(st.session_state.retriever, query)
+        st.chat_message("ai").write(response)
+
+if __name__ == '__main__':
+    #
+    boot()
+    
